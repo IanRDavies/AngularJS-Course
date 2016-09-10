b8777eaf50aebdcea39e1ddb0bffa613d19b339c
IanRDavies <ian_davies_@hotmail.co.uk>
2016-09-10 15:15:26 +0100

    Commented code for readability

--- Assignment-1/shared/app.js
+++ Assignment-1/shared/app.js
@@ -4,17 +4,21 @@
 angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject = ['$scope'] //protection from minification
+//protection from minification
+LunchCheckController.$inject = ['$scope']
+
+// Setting up the controller
 function LunchCheckController($scope) {
+	// initialisiing the messages and the input to empty strings
 	$scope.message = "";
 	$scope.menu = "";
 	$scope.mealCheck = function() {
 		$scope.menu = $scope.menu.trim();
-		if(!$scope.menu) {
+		if(!$scope.menu) { // if whitespace/nothing entered
 			$scope.message = "Please enter data first";
 			$scope.changeTextColor("red");
 			$scope.changeBoxColor("red");
-		} else {
+		} else { // if some list of items is entered
 				$scope.menuList = $scope.menu.split(",");
 				$scope.menuLength = $scope.menuList.length;
 				$scope.changeTextColor("green");
@@ -26,6 +30,7 @@ function LunchCheckController($scope) {
 				}
 		}
 };
+    // Section for the extension tasks - changing the style functions (look at ng-style tags in the HTML)
 	$scope.messageStyle = {};
 	$scope.changeTextColor = function(color) {
 		$scope.messageStyle.style = {'color':color};
